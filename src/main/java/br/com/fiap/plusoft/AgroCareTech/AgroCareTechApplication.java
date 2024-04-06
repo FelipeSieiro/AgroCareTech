04031ee9728cf2e9e701c11d15988fbffca18d53
FelipeSieiro <felipedossantos0804@gmail.com>
2024-04-06 12:25:00 -0300

    feat: add spring in project

---
+++ src/main/java/br/com/fiap/plusoft/AgroCareTech/AgroCareTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.plusoft.AgroCareTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroCareTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroCareTechApplication.class, args);
+	}
+
+}
